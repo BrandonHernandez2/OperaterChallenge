76334c082257a003330b6687e3001f7941fa8b38
hernabra000 <hernabra000@warren.k12.in.us>
2024-08-09 13:16:33 -0400

    finished operator challenge

--- src/OperatorChall.java
+++ src/OperatorChall.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class OperatorChall {
+public class OperatorChall {
+    public static void main(String[] args) {
+
+        double myDouble = 20.00;
+        double mySecondDouble = 80.00;
+
+        double resultOfDoubles = (myDouble + mySecondDouble) * 100.00;
+        System.out.println("The Result of the Doubles is: " + resultOfDoubles);
+
+        double remainderOfDoubles = resultOfDoubles % 40.00;
+        System.out.println("The result of the Double: " + remainderOfDoubles);
+
+        boolean remainderResult = (remainderOfDoubles == 0) ? true : false;
+        System.out.println("The remainder is: "  + remainderResult);
+
+        if (!remainderResult) {
+            System.out.println("got some remainder");
+        }
+
+
+
+    }
 }
